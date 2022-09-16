9b52dd1da8b9b2709e439ad117917eb359a58c33
David <dr.pestana@campus.fct.unl.pt>
2022-09-16 22:56:54 +0100

    GN funcional

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GN          = "GN";
+
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -53,6 +55,9 @@ public class Main {
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
                     break;
+                case GN:
+                    gn(in, cBook);
+                    break;
                 default:
                     System.out.println(COMMAND_ERROR);
             }
@@ -62,6 +67,13 @@ public class Main {
         System.out.println(QUIT_MSG);
         System.out.println();
         in.close();
+    }
+
+    private static void gn(Scanner in, ContactBook cBook) {
+        int number = in.nextInt(); in.nextLine();
+        if (cBook.hasContact(number))
+            System.out.println(cBook.getName(number));
+        else System.out.println("Phone number does not exist.");
     }
 
     private static String getCommand(Scanner in) {
